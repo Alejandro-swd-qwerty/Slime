2ee4013a9d43894dec4e425eb90fdc700c23d4d1
Alejandro <c.alejandro.83@hotmail.com>
2019-10-24 10:43:29 -0500

    [MOD] Modifying header and cards on Dsk view

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
-$(".dropdown").click(function(){
-  $(".hiden-content").hide();
-//  $(".hiden-content").toggle(1000);
+$(".hidden-content").hide();
+
+$("#dropdown-btn").click(() =>{
+  $(".hidden-content").toggle(1000);
 });
-/*
-$("#dropdown-btn").click(function(){
-  $("hiden-content").show();
-});
-*/
-/*
-$(".hiden-content").hide();
-
-      $("#dd_heard_learn_about_004").change(function() {
-
-    $(".hiden-content").hide();
-
-      $(".short_field_001" + $(this).val()).show();
-
-    });
-    */
